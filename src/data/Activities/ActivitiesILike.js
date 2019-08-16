e6b2489be8981d1431189229371bd7f7b393d42c
afozbek <furkanozbek1995@gmail.com>
2019-08-16 20:32:05 +0300

    activities I like

---
+++ src/data/Activities/ActivitiesILike.js
@@ -0,0 +1,14 @@
+export const activitiesILike = [
+    { id: 1, activityDesc: "Problem çözmek" },
+    { id: 2, activityDesc: "Bildiklerimi başkasına anlatmak" },
+    { id: 3, activityDesc: "Yeni projeler üretmek" },
+    { id: 4, activityDesc: "Yeni insanlarla tanışmak" },
+    { id: 5, activityDesc: "Planlı ve programlı çalışmak" },
+    {
+        id: 6,
+        activityDesc:
+            "Çevremdeki insanların problemlerini dinleyip çözüm önerisi sunmak"
+    },
+    { id: 7, activityDesc: "Arkadaşlarımla dolaşmak ve sohbet etmek" },
+    { id: 8, activityDesc: "Film izlemek" }
+];
