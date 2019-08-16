3879e274f40267ee0831b37990f1543fc2c4d4af
afozbek <furkanozbek1995@gmail.com>
2019-08-16 20:30:43 +0300

    updating Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,46 +1,43 @@
 import React, { useState } from "react";
 
 import "./Footer.css";
+import Activities from "./Activities/Activities";
+import ShortMe from "./WhoAmI/ShortMe";
+import LongMe from "./WhoAmI/LongMe";
+import Buttons from "./Buttons/Buttons";
 
 const Footer = () => {
-    const [clickedButton, setClickedButton] = useState("whoAmI");
+    const [shortMe, longMe, whatILike] = ["shortMe", "longMe", "whatILike"];
+    const [clickedButton, setClickedButton] = useState(shortMe);
 
-    const [whoAmI, whatILike] = ["whoAmI", "whatILike"];
+    const shortMeClickHandler = e => {
+        setClickedButton(shortMe);
+    };
 
-    const whoAmIClickHandler = e => {
+    const longMeClickHandler = e => {
+        setClickedButton(longMe);
+    };
+
+    const whatILikeClickHandler = e => {
         setClickedButton(whatILike);
     };
-    const whatILikeClickHandler = e => {
-        setClickedButton(whoAmI);
-    };
+
+    let renderedContent = <ShortMe />;
+
+    if (clickedButton === longMe) {
+        renderedContent = <LongMe />;
+    } else if (clickedButton === whatILike) {
+        renderedContent = <Activities />;
+    }
 
     return (
         <div className="footer">
-            <div className="footer__buttons">
-                {clickedButton === "whoAmI" ? (
-                    <button onClick={whoAmIClickHandler}>Who Am I</button>
-                ) : (
-                    <button onClick={whatILikeClickHandler}>What I Like</button>
-                )}
-            </div>
-            <div className="footer__short">
-                <p>
-                    Hi! I am Furkan
-                    <span role="img" aria-label="hi">
-                        🖐🏻
-                    </span>
-                </p>
-                <p>
-                    I'm a software developer who likes to solve my problems with
-                    code. My overall goal is to build performance systems and
-                    develop stable applications.
-                </p>
-                <p>
-                    I am interested in web technologies and develop projects in
-                    this field. I am especially familiar with
-                    <strong> Reactjs, Reduxjs and Nodejs.</strong>
-                </p>
-            </div>
+            <Buttons
+                shortMeClickHandler={shortMeClickHandler}
+                longMeClickHandler={longMeClickHandler}
+                whatILikeClickHandler={whatILikeClickHandler}
+            />
+            {renderedContent}
         </div>
     );
 };
