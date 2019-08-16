fb03f247d6db5da0ee2bf5183179f4f99368a520
afozbek <furkanozbek1995@gmail.com>
2019-08-16 12:12:35 +0300

    Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Furkan from "../../assets/images/furkan-ozbek.jpeg";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__img">
+                <img src={Furkan} alt="Furkan" />
+            </div>
+
+            <div className="header__desc">
+                <h1>Abdullah Furkan Özbek</h1>
+                <h3>Front End Developer</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
