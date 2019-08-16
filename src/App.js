6a5ba5016e27b819cad10279e2ab379a94a9926c
afozbek <furkanozbek1995@gmail.com>
2019-08-16 12:10:41 +0300

    adding Container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 
+import Header from "./Components/Header/Header";
+import Links from "./Components/Links/Links";
+import Footer from "./Components/Footer/Footer";
+
+import "./App.css";
+
 export default class App extends Component {
     render() {
-        return <div>Hello World</div>;
+        return (
+            <div className="container">
+                <Header />
+                <Links />
+                <Footer />
+            </div>
+        );
     }
 }
