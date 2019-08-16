5d2f78083439a348f506e5a9a39d47e3bbfb7025
afozbek <furkanozbek1995@gmail.com>
2019-08-16 20:31:43 +0300

    Short Me

---
+++ src/Components/Footer/WhoAmI/ShortMe.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ShortMe = () => {
+    return (
+        <div className="footer__whoAmI">
+            <p>
+                Hi! I am Furkan
+                <span role="img" aria-label="hi">
+                    🖐🏻
+                </span>
+            </p>
+            <p>
+                I am a front end developer who likes to solve my problems with
+                code. My overall goal is to build performance systems and to
+                develop stable applications.
+            </p>
+            <p>
+                I am interested in web technologies and develop projects in this
+                field. I am especially familiar with
+                <strong> Reactjs, Reduxjs and Nodejs.</strong>
+            </p>
+        </div>
+    );
+};
+
+export default ShortMe;
