946f4f0fa3904c6ce273d38151c95c722d1ae364
afozbek <furkanozbek1995@gmail.com>
2019-08-16 21:12:51 +0300

    added mail link

--- src/Components/Links/Links.js
+++ src/Components/Links/Links.js
@@ -5,13 +5,20 @@ import "./Links.css";
 import { socialLinks } from "./SocialLinks";
 
 const Links = () => {
-    const { githubUrl, twitterUrl, linkedinUrl, codepenUrl } = socialLinks;
+    const {
+        githubUrl,
+        twitterUrl,
+        linkedinUrl,
+        codepenUrl,
+        mailUrl
+    } = socialLinks;
     return (
         <div className="social-links">
             <Link href={twitterUrl}>Twitter</Link>
             <Link href={githubUrl}>Github</Link>
             <Link href={linkedinUrl}>Linkedin</Link>
             <Link href={codepenUrl}>Codepen</Link>
+            <Link href={mailUrl}>Email</Link>
         </div>
     );
 };
