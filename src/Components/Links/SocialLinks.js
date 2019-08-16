acbbfb99f989d75dcb238c011d324a7dae4b51d1
afozbek <furkanozbek1995@gmail.com>
2019-08-16 20:31:54 +0300

    updating social links

--- src/Components/Links/SocialLinks.js
+++ src/Components/Links/SocialLinks.js
@@ -2,5 +2,6 @@ export const socialLinks = {
     twitterUrl: "https://twitter.com/afozbek_",
     githubUrl: "https://github.com/afozbek",
     codepenUrl: "https://codepen.io/afozbek",
-    linkedinUrl: "https://linkedin.com/in/afozbek"
+    linkedinUrl: "https://linkedin.com/in/afozbek",
+    mailUrl: "mailto://furkanozbek1995@gmail.com"
 };
