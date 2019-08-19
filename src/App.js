5fc94e33355c2a605f56ab66994a657e36dc7736
afozbek <furkanozbek1995@gmail.com>
2019-08-19 15:54:15 +0300

    adding fun animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Header from "./Components/Header/Header";
 import Links from "./Components/Links/Links";
 import Footer from "./Components/Footer/Footer";
+import { cornify_add } from "./util/Cornify";
 
 import "./App.css";
 
-export default class App extends Component {
+class App extends React.Component {
+    state = {
+        keyArr: [],
+        secret: "furkan"
+    };
+
+    componentDidMount() {
+        console.log("Effectingg");
+        this.keyUpListener = window.addEventListener(
+            "keyup",
+            this.keyUpEventListener
+        );
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener(this.keyUpListener);
+    }
+
+    keyUpEventListener = e => {
+        let newArr;
+        if (this.state.keyArr.length === this.state.secret.length) {
+            newArr = [...this.state.keyArr.slice(1), e.key];
+        }
+
+        this.setState(prevState => ({
+            keyArr: newArr ? [...newArr] : [...prevState.keyArr, e.key]
+        }));
+    };
+
     render() {
+        console.log(this.state.keyArr);
+
+        if (this.state.keyArr.join("").includes(this.state.secret)) {
+            cornify_add();
+        }
+
         return (
             <div className="container">
                 <Header />
@@ -17,3 +52,4 @@ export default class App extends Component {
         );
     }
 }
+export default App;
